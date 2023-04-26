e5277e330f0d9817769c4f1abbfa63db1abfbd0f
gtran6 <gtran6@depaul.edu>
2023-04-26 14:57:51 -0500

    modify alert view model

--- app/src/main/java/com/myprojects/ui/alert/AlertViewModel.kt
+++ app/src/main/java/com/myprojects/ui/alert/AlertViewModel.kt
@@ -1,7 +1,33 @@
 package com.myprojects.ui.alert
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.myprojects.data.entity.AlertEntity
+import com.myprojects.data.remote.Resource
+import com.myprojects.repository.RepositoryInterface
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class AlertViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AlertViewModel(val repo: RepositoryInterface) : ViewModel() {
+    fun insetAlert(alertEntity: AlertEntity) {
+        viewModelScope.launch {
+            repo.insertAlert(alertEntity)
+        }
+    }
+
+    fun getAllAlerts = liveData(Dispatchers.IO) {
+        emit(Resource.Loading(isLoading = true, _data = null))
+        try {
+            emit(Resource.Success(_data = repo.getAllAlerts()))
+        } catch (e: Exception) {
+            Resource.Error(exception = e.message ?: "Error")
+        }
+    }
+
+    fun removeAlert(alertEntity: AlertEntity) {
+        viewModelScope.launch {
+            repo.deleteAlert(alertEntity)
+        }
+    }
 }
