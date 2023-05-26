aa98775f3895ddd5f180a5b092c12f5e606b18b4
gtran6 <gtran6@depaul.edu>
2023-05-26 18:01:02 -0500

    fav working

--- app/src/main/java/com/myprojects/ui/map/MapsFragment.kt
+++ app/src/main/java/com/myprojects/ui/map/MapsFragment.kt
@@ -8,7 +8,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.gms.maps.CameraUpdateFactory
