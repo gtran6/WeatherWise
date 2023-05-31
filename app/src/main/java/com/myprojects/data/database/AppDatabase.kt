41a0d1ddfcc9bc17bbb0932c7a402f5666f8b62d
gtran6 <gtran6@depaul.edu>
2023-05-30 20:01:42 -0500

    fav working

--- app/src/main/java/com/myprojects/data/database/AppDatabase.kt
+++ app/src/main/java/com/myprojects/data/database/AppDatabase.kt
@@ -1,8 +1,6 @@
 package com.myprojects.data.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.myprojects.data.DAO.AlertDao
